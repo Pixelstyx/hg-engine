370dbd6e55de5f27fded84195e5c98ce07c75a74
Pixelstyx <jsparxs@gmail.com>
2025-12-19 20:03:37 -0500

    Convert diamond dust check to switch statement

--- src/field_system_rtc_weather.c
+++ src/field_system_rtc_weather.c
@@ -35,6 +35,24 @@ u32 Fsys_GetWeather_HandleDiamondDust(FieldSystem *fieldSystem, u32 mapId) {
             }
             return weatherType;
         }
+    switch (fieldSystem->location->mapId)
+    {
+        case MAP_MOUNT_SILVER_CAVE_SUMMIT: // MAP_D41R0108
+            for (c = 0; c < NELEMS(DiamondDustDates); c++) 
+            {
+                u8 month = DiamondDustDates[c].month;
+                u8 day = DiamondDustDates[c].day;
+                if ((sysinfo_rtc->date.month == month) && (sysinfo_rtc->date.day == day)) 
+                {
+                    if (!FieldSystem_HasPenalty(fieldSystem)) 
+                    {
+                        weatherType = WEATHER_SYS_DIAMOND_DUST;
+                    }
+                    return weatherType;
+                }
+            }
+            break;
+        default: break;
     }
     return weatherType;
 }
