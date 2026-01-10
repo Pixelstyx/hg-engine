b1facb658e892b1be55968d6071c19291c5f8a04
Pixelstyx <jsparxs@gmail.com>
2026-01-10 15:12:50 -0500

    Expose everything

--- include/rock_smash_item.h
+++ include/rock_smash_item.h
@@ -1,7 +1,7 @@
 #ifndef ROCK_SMASH_ITEM_H
 #define ROCK_SMASH_ITEM_H
 
-// #include "field_system.h"
+#include "battle.h"
 
 #define ROCK_SMASH_TABLE_DEFAULT        0
 #define ROCK_SMASH_TABLE_RUINS_OF_ALPH  1
@@ -9,23 +9,22 @@
 
 #define NUM_ROCK_SMASH_TABLES           3
 
-typedef struct {
-    u16 odds;
-    u16 table;
-} ROCK_SMASH;
+void LONG_CALL FieldSystem_RockSmashItemCheck(FieldSystem *fieldSystem, int followMonKnowsHm, u16 *itemFound, u16 *item);
 
-typedef struct {
-    BOOL followMonKnowsHM;
-    int ability;
-    u16 *itemFound; // TODO: bool16
-    u16 *item;
-    ROCK_SMASH rockSmash;
-    u32 unk14;
-} RockSmashItemCheckWork;
+// TODO: Move this somewhere better (filesystem.h does not exist yet).
+void LONG_CALL ReadWholeNarcMemberByIdPair(void *dest, u32 narc_id, s32 file_id);
 
-static BOOL CheckRockSmashItemDrop(FieldSystem *fieldSystem, RockSmashItemCheckWork *env);
+// TODO: Move this somewhere better (overlay_02.h does not exist).
+BOOL LONG_CALL ov02_022470A0(FieldSystem *fieldSystem, struct BattleSetup **setupPtr);
 
-// TODO: Move this somewhere better.
-void ReadWholeNarcMemberByIdPair(void *dest, u32 narc_id, s32 file_id);
+// TODO: Move this somewhere better (encounter.h does not exist).
+void LONG_CALL FieldSystem_StartForcedWildBattle(FieldSystem *fieldSystem, TaskManager *taskManager, struct BattleSetup *setup);
+
+// TODO: Move this somewhere better (overlay_01_022001E4.h does not exist).
+int LONG_CALL ov01_02200540(LocalMapObject *obj, int a0, int a1);
+BOOL LONG_CALL ov01_022003F4(int a0);
+
+// TODO: Move this somewhere better (unk_020689C8.h does not exist).
+void LONG_CALL sub_02068B48(int a0);
 
 #endif // ROCK_SMASH_ITEM_H
--- src/field/rock_smash_item.c
+++ src/field/rock_smash_item.c
@@ -1,11 +1,99 @@
 #include "../../include/constants/ability.h"
+#include "../../include/constants/file.h"
 #include "../../include/constants/item.h"
 #include "../../include/constants/maps.h"
 #include "../../include/battle.h"
 #include "../../include/debug.h"
+#include "../../include/map_events_internal.h"
 #include "../../include/rock_smash_item.h"
 
-static BOOL CheckRockSmashItemDrop(FieldSystem *fieldSystem, RockSmashItemCheckWork *env) {
+typedef struct {
+    u16 odds;
+    u16 table;
+} ROCK_SMASH;
+
+typedef struct {
+    BOOL followMonKnowsHM;
+    int ability;
+    u16 *itemFound; // TODO: bool16
+    u16 *item;
+    ROCK_SMASH rockSmash;
+    u32 unk14;
+} RockSmashItemCheckWork;
+
+static const u16 sRockSmashItems_Default[] = {
+    ITEM_MAX_ETHER,
+    ITEM_REVIVE,
+    ITEM_HEART_SCALE,
+    ITEM_RED_SHARD,
+    ITEM_BLUE_SHARD,
+    ITEM_GREEN_SHARD,
+    ITEM_YELLOW_SHARD,
+    ITEM_STAR_PIECE,
+};
+
+static const u16 sRockSmashItems_RuinsOfAlph[] = {
+    ITEM_RED_SHARD,
+    ITEM_YELLOW_SHARD,
+    ITEM_HELIX_FOSSIL,
+    ITEM_MAX_ETHER,
+    ITEM_BLUE_SHARD,
+    ITEM_GREEN_SHARD,
+    ITEM_OLD_AMBER,
+    ITEM_MAX_REVIVE,
+};
+
+static const u16 sRockSmashItems_CliffCave[] = {
+    ITEM_MAX_ETHER,
+    ITEM_PEARL,
+    ITEM_BIG_PEARL,
+    ITEM_RED_SHARD,
+    ITEM_YELLOW_SHARD,
+    ITEM_CLAW_FOSSIL,
+    ITEM_CLAW_FOSSIL,
+    ITEM_RARE_BONE,
+};
+
+static BOOL LONG_CALL Task_RockSmashItemCheck(TaskManager *taskman);
+static BOOL LONG_CALL CheckRockSmashItemDrop(FieldSystem *fieldSystem, RockSmashItemCheckWork *env);
+static BOOL LONG_CALL Task_GetRockSmashItem(TaskManager *taskman);
+static int LONG_CALL DrawRockSmashIdx(FieldSystem *fieldSystem);
+
+void LONG_CALL FieldSystem_RockSmashItemCheck(FieldSystem *fieldSystem, int followMonKnowsHm, u16 *itemFound, u16 *item) {
+    RockSmashItemCheckWork *env = sys_AllocMemoryLo(HEAPID_WORLD, sizeof(RockSmashItemCheckWork));
+    env->followMonKnowsHM = followMonKnowsHm != 0;
+    env->itemFound = itemFound;
+    env->item = item;
+    *env->itemFound = FALSE;
+    *env->item = ITEM_NONE;
+    TaskManager_Call(fieldSystem->taskman, Task_RockSmashItemCheck, env);
+}
+
+static BOOL LONG_CALL Task_RockSmashItemCheck(TaskManager *taskman) {
+    struct BattleSetup *setup;
+    FieldSystem *fieldSystem = taskman->fieldSystem;
+    RockSmashItemCheckWork *env = taskman->env;
+
+    if (ov02_022470A0(fieldSystem, &setup)) {
+        *env->itemFound = FALSE;
+        *env->item = ITEM_NONE;
+        sys_FreeMemoryEz(env);
+        FieldSystem_StartForcedWildBattle(fieldSystem, taskman, setup);
+        return FALSE;
+    }
+
+    if (CheckRockSmashItemDrop(fieldSystem, env)) {
+        TaskManager_Jump(taskman, Task_GetRockSmashItem, env);
+        return FALSE;
+    }
+
+    *env->itemFound = FALSE;
+    *env->item = ITEM_NONE;
+    sys_FreeMemoryEz(env);
+    return TRUE;
+}
+
+static BOOL LONG_CALL CheckRockSmashItemDrop(FieldSystem *fieldSystem, RockSmashItemCheckWork *env) {
     int odds;
     int ability;
     ROCK_SMASH data;
@@ -69,4 +157,87 @@ static BOOL CheckRockSmashItemDrop(FieldSystem *fieldSystem, RockSmashItemCheckW
         return TRUE;
     }
     return FALSE;
+}
+
+static BOOL LONG_CALL Task_GetRockSmashItem(TaskManager *taskman) {
+    LocalMapObject *obj;
+
+    u32 *state_p = &taskman->state;
+    RockSmashItemCheckWork *env = taskman->env;
+    FieldSystem *fieldSystem = taskman->fieldSystem;
+
+    switch (*state_p) {
+    case 0:
+        if (env->followMonKnowsHM) {
+            obj = fieldSystem->followMon.mapObject;
+        } else {
+            obj = fieldSystem->playerAvatar->mapObject;
+        }
+        env->unk14 = ov01_02200540(obj, 0, 1);
+        ++(*state_p);
+        break;
+    case 1:
+        if (ov01_022003F4(env->unk14)) {
+            sub_02068B48(env->unk14);
+            ++(*state_p);
+        }
+        break;
+    case 2: {
+        u32 idx = DrawRockSmashIdx(fieldSystem);
+        if ((env->ability == ABILITY_SERENE_GRACE || env->ability == ABILITY_SUPER_LUCK) && idx < 7) {
+            idx = (u8)(idx + 1);
+        }
+        GF_ASSERT(idx < 8);
+
+        u16 item;
+        switch (env->rockSmash.table) {
+        case ROCK_SMASH_TABLE_DEFAULT:
+            item = sRockSmashItems_Default[idx];
+            break;
+        case ROCK_SMASH_TABLE_RUINS_OF_ALPH:
+            item = sRockSmashItems_RuinsOfAlph[idx];
+            break;
+        case ROCK_SMASH_TABLE_CLIFF_CAVE:
+            item = sRockSmashItems_CliffCave[idx];
+            break;
+        default:
+            GF_ASSERT(FALSE);
+            *env->itemFound = FALSE;
+            *env->item = ITEM_NONE;
+            sys_FreeMemoryEz(env);
+            return TRUE;
+        }
+        *env->itemFound = TRUE;
+        *env->item = item;
+        sys_FreeMemoryEz(env);
+        return TRUE;
+    }
+    }
+    return FALSE;
+}
+
+static int LONG_CALL DrawRockSmashIdx(UNUSED FieldSystem *fieldSystem) {
+    u8 rand = gf_rand() % 100;
+    if (rand < 25) {
+        return 0;
+    }
+    if (rand < 45) {
+        return 1;
+    }
+    if (rand < 55) {
+        return 2;
+    }
+    if (rand < 65) {
+        return 3;
+    }
+    if (rand < 75) {
+        return 4;
+    }
+    if (rand < 85) {
+        return 5;
+    }
+    if (rand < 95) {
+        return 6;
+    }
+    return 7;
 }
