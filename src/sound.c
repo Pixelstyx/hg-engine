5e7844a9ad7ec3d7303c3e0f073963115630e802
Pixelstyx <jsparxs@gmail.com>
2026-07-04 22:53:23 -0400

    half implement soundplate fix

--- src/sound.c
+++ src/sound.c
@@ -239,13 +239,13 @@ int LONG_CALL NNSi_SndArcLoadBank(int bankNo, u32 loadFlag, void *heap, BOOL bSe
 
 // ALL THAT SOUNDPLATE NONSENSE GOES BELOW HERE.
 
-u8 sBGMVolume[3] = { // ov01_02206388
+const u8 sBGMVolume[3] = {
     0x60,
     0x40,
-    0x20,
+    0x20
 };
 
-u8 sSoundplateVolume[16][3] = { // ov01_0220638C
+const u8 sSoundplateVolume[16][3] = {
     {0x40, 0x60, 0x7F},
     {0x2E, 0x60, 0x7F},
     {0x2E, 0x60, 0x7F},
@@ -264,7 +264,30 @@ u8 sSoundplateVolume[16][3] = { // ov01_0220638C
     {0x2E, 0x60, 0x7F}
 };
 
-u16 gSoundplateSounds[16][2] = { // ov01_022063BC
+enum SoundplateSoundParams {
+    SOUNDPLATE_SOUND_SEQ = 0,
+    SOUNDPLATE_SOUND_UNK_BOOL,
+    SOUNDPLATE_SOUND_PARAMS
+};
+
+typedef struct Soundplate {
+    u8 soundplateSoundID;
+    u8 volumeIndex;
+    u8 unk2;
+    u8 unk3;
+    u8 x;
+    u8 z;
+    u8 xBounds;
+    u8 zBounds;
+} Soundplate;
+
+typedef struct SoundplateStruct {
+    u8 unk0[2];
+    vu16 unk2; // fakematch?
+    Soundplate soundplates[];
+} SoundplateStruct;
+
+const u16 sSoundplateSounds[16][SOUNDPLATE_SOUND_PARAMS] = {
     { SEQ_SE_GS_N_SESERAGI,     TRUE  }, // Water Flow
 	{ SEQ_SE_GS_N_HUUSHA,       FALSE }, // Windmill
 	{ SEQ_SE_GS_N_UMIBE,        FALSE }, // Seashore
@@ -280,23 +303,18 @@ u16 gSoundplateSounds[16][2] = { // ov01_022063BC
 	{ SEQ_SE_GS_KYOUHUU,        TRUE  }, // Strong Wind
 	{ SEQ_SE_GS_N_ENGINE,       TRUE  }, // Engine
 	{ SEQ_SE_GS_N_HUNSUI,       FALSE }, // Fountain
-	{ SEQ_SE_GS_DENGEKIBARIA,   FALSE }, // Electric Barrier
+	{ SEQ_SE_GS_DENGEKIBARIA,   FALSE }  // Electric Barrier
 };
 
-typedef struct UnkSoundplateStruct {
-    u32 unk0;
-    u8 unk4[][8];
-} UnkSoundplateStruct;
+int LONG_CALL GetLocalSoundplateID(SoundplateStruct *soundplateStruct, int globalX, int globalZ);
+BOOL LONG_CALL ov01_021E7D58(FieldSystem *fieldSystem, SoundplateStruct *soundplateStruct, int soundplateID);
+void LONG_CALL sub_02006088(u16 sndseq);
+SoundplateStruct LONG_CALL *sub_02054874(FieldSystem *fieldSystem, int x, int z);
+void LONG_CALL GF_SndHandleMoveVolume(int handle, int volume, int);
+void LONG_CALL StopSE(u16 sndseq, int a1);
 
-int ov01_021E7D00(UnkSoundplateStruct*, u32 x, u32 z);
-BOOL ov01_021E7D58(FieldSystem *fieldSystem, UnkSoundplateStruct*, s32);
-void sub_02006088(u16 sndseq);
-UnkSoundplateStruct *sub_02054874(FieldSystem *fieldSystem, u32 x, u32 z);
-void GF_SndHandleMoveVolume(int handle, int volume, int);
-void StopSE(u16 sndseq, int a1);
-
-void ov01_021E7DFC(struct FieldSystem *fieldSystem, u32 x, s32 z) {
-    UnkSoundplateStruct *temp_r6 = sub_02054874(fieldSystem, x, z);
+/*void LONG_CALL ov01_021E7DFC(FieldSystem *fieldSystem, int x, int z) {
+    SoundplateStruct *soundplateStruct = sub_02054874(fieldSystem, x, z);
     
     if (fieldSystem->unkC4 == -2) {
         fieldSystem->unkC4 = -1;
@@ -304,25 +322,23 @@ void ov01_021E7DFC(struct FieldSystem *fieldSystem, u32 x, s32 z) {
         fieldSystem->unkC4 = -1;
     }
     
-    z = ov01_021E7D00(temp_r6, x, z); // This is not actually z anymore but this needs to be set for matching.
+    z = GetLocalSoundplateID(soundplateStruct, x, z);
     if (z != -1) {
-        if (ov01_021E7D58(fieldSystem, temp_r6, z)) {
-            // u8 soundIndex = temp_r6->unk4[z][0];
-            if (temp_r6->unk4[z][0] < 16) {
-                if (fieldSystem->unkC4 != gSoundplateSounds[temp_r6->unk4[z][0]][0]) {
-                    debug_printf("FieldSystem unkC4: %d\n", fieldSystem->unkC4);
-                    debug_printf("Soundplate: %d\n", gSoundplateSounds[temp_r6->unk4[z][0]]);
-                    if (gSoundplateSounds[temp_r6->unk4[z][0]][1] == TRUE) {
-                        sub_02006088(gSoundplateSounds[temp_r6->unk4[z][0]][0]);
+        if (ov01_021E7D58(fieldSystem, soundplateStruct, z)) {
+            u8 soundplateSoundID = soundplateStruct->soundplates[z].soundplateSoundID;
+            if (soundplateSoundID < 16) {
+                if (fieldSystem->unkC4 != sSoundplateSounds[soundplateSoundID][SOUNDPLATE_SOUND_SEQ]) {
+                    if (sSoundplateSounds[soundplateSoundID][SOUNDPLATE_SOUND_UNK_BOOL] == TRUE) {
+                        sub_02006088(sSoundplateSounds[soundplateSoundID][SOUNDPLATE_SOUND_SEQ]);
                     } else {
-                        PlaySE(gSoundplateSounds[temp_r6->unk4[z][0]][0]);
+                        PlaySE(sSoundplateSounds[soundplateSoundID][SOUNDPLATE_SOUND_SEQ]);
                     }
                 }
-                fieldSystem->unkC4 = gSoundplateSounds[temp_r6->unk4[z][0]][0];
-                // u8 volumeIndex = temp_r6->unk4[z][1];
-                if (temp_r6->unk4[z][1] < 3) {
-                    GF_SndHandleMoveVolume(0, sBGMVolume[temp_r6->unk4[z][1]], 15);
-                    GF_SndHandleMoveVolume(5, sSoundplateVolume[temp_r6->unk4[z][0]][temp_r6->unk4[z][1]], 5);
+                fieldSystem->unkC4 = sSoundplateSounds[soundplateStruct->soundplates[z].soundplateSoundID][SOUNDPLATE_SOUND_SEQ];
+                u8 volumeIndex = soundplateStruct->soundplates[z].volumeIndex;
+                if (volumeIndex < 3) {
+                    GF_SndHandleMoveVolume(0, sBGMVolume[volumeIndex], 15);
+                    GF_SndHandleMoveVolume(5, sSoundplateVolume[soundplateStruct->soundplates[z].soundplateSoundID][soundplateStruct->soundplates[z].volumeIndex], 5);
                 }
             }
         }
@@ -333,4 +349,4 @@ void ov01_021E7DFC(struct FieldSystem *fieldSystem, u32 x, s32 z) {
             fieldSystem->unkC4 = -1;
         }
     }
-}
+}*/
